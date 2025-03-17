ba972e1182628906e915c1416c423f4b91953f39
raphael <raphael.moynat@gmail.com>
2025-03-17 14:27:52 +0100

    router

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,27 @@
 import './App.css'
 import MovieCard from "./components/MovieCard";
 import Home from "./pages/Home"
+import Favorite from "./pages/Favorite"
+import {Route, Routes} from "react-router-dom";
+import Navbar from "./components/NavBar";
+
 
 function App() {
-    const movieNumber = 2
 
 
   return (
       <>
-         <Home />
+          <div>
+              <Navbar></Navbar>
+          </div>
+         <main className="main-content">
+             <Routes>
+                 <Route path="/" element={<Home />} />
+                 <Route path="/favorite" element={<Favorite />} />
+
+             </Routes>
+
+         </main>
 
 
       </>
---
+++ src/pages/Favorite.jsx
@@ -0,0 +1,12 @@
+
+
+function Favorite() {
+    return (
+        <div className="favorite-empty">
+            <h1>No favorite Movies Yet</h1>
+            <p>start adding movies</p>
+        </div>
+    )
+}
+
+export default Favorite
